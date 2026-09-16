bcbab4f8034e01e7270e7a8d95843cccd9bd0a3e
FranzS <khamenia.matvey@gmail.com>
2026-09-16 14:37:49 +0300

    add: gene data & RLE Decode

--- Program.cs
+++ Program.cs
@@ -6,19 +6,31 @@ namespace lab1;
 class Program
 {
     struct Command
-    {
-        public enum Operation { Search, Diff, Mode };
-        public Operation operation;
-        public string[] parametrs;
-    };
+        {
+            public enum Operation { Search, Diff, Mode };
+            public Operation operation;
+            public string[] parametrs;
+        };
 
-    struct Gene_data
-    {
-        public string protein;
-        public string organism;
-        public string amino;
-    };
+        struct Gene_data
+        {
+            public string protein;
+            public string organism;
+            public string amino;
+        };
 
+        static string RLDecode(string sequance)
+        {
+            StringBuilder decoded = new();
+            for (int i = 0; i < sequance.Length; i++)
+            {
+                int count = 1;
+                if (sequance[i] >= '3' && sequance[i] <= '9')
+                { count = sequance[i++] - '0'; }
+                decoded.Append(sequance[i], count);
+            }
+            return decoded.ToString();
+        }
 
 
     public static void Main()
