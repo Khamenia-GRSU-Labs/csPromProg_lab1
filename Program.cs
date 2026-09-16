b441c6da950cedcb13479438b296cf43e32732f0
FranzS <khamenia.matvey@gmail.com>
2026-09-16 14:34:03 +0300

    setup project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text;
+
+namespace lab1;
+
+class Program
+{
+    struct Command
+    {
+        public enum Operation { Search, Diff, Mode };
+        public Operation operation;
+        public string[] parametrs;
+    };
+
+    struct Gene_data
+    {
+        public string protein;
+        public string organism;
+        public string amino;
+    };
+
+
+
+    public static void Main()
+    {
+
+
+    }
+}
